c3360924e6eea6417b6420dbc01c0237e1e46e1b
ayush-kartrocket <ayush.sharma@kartrocket.com>
2020-12-10 10:59:42 +0530

    name changes

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux_1';
 import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) =>{
